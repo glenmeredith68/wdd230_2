c9f0453195e65067de0ec6672934dfef0d44aa78
Glen Meredith <mer16014@byui.edu>
2022-03-18 21:46:18 -0600

    prophets.html JSON activity

---
+++ w09/js/prophets.js
@@ -0,0 +1,31 @@
+const url = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        const prophets = data['prophets'];
+        prophets.forEach(prophet => {
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let portrait = document.createElement('img');
+            let bday = document.createElement('p');
+            let bplace = document.createElement('p');
+
+            h2.textContent = `${prophet.name} ${prophet.lastname}`;
+
+            portrait.setAttribute('src', prophet.imageurl);
+            portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname} - Latter-day President #${prophet.order}.`);
+            portrait.setAttribute('loading', 'lazy');
+
+            bday.textContent = `${prophet.name} ${prophet.lastname} was born on ${prophet.birthdate}.`;
+            bplace.textContent = `President ${prophet.lastname} was born in ${prophet.birthplace}.`;
+
+            card.appendChild(h2);
+            card.appendChild(portrait);
+            card.appendChild(bday);
+            card.appendChild(bplace);
+
+            document.querySelector('.cards').appendChild(card);
+
+        });
+    });
