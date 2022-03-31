d1495840a13e32da8558e2e23d16c05c71ac5c84
Glen Meredith <mer16014@byui.edu>
2022-03-30 20:40:25 -0600

    nav dropdown works

--- temple-inn/js/script.js
+++ temple-inn/js/script.js
@@ -4,7 +4,9 @@ let services = document.querySelector('.services');
 let dropdown = document.querySelector('.drop');
 services.addEventListener('mouseover', () => {
     dropdown.style.display = 'flex';
+    dropdown.style.opacity = '1';
 })
 services.addEventListener('mouseout', () => {
     dropdown.style.display = 'none';
+    dropdown.style.opacity = '0';
 })
