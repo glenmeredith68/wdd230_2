f60a89c627b57d81c86d9743684339f283e293f6
Glen Meredith <mer16014@byui.edu>
2022-04-04 12:33:04 -0600

    instagram divs js

--- temple-inn/js/script.js
+++ temple-inn/js/script.js
@@ -16,3 +16,31 @@ services.addEventListener('mouseout', () => {
 let outputSpan = document.querySelector('#outputspan');
 let year = new Date().getFullYear();
 outputSpan.textContent = year;
+
+
+// instagram posts
+
+const leftBtn = document.querySelector('.left-btn');
+const rightBtn = document.querySelector('.right-btn');
+const post1 = document.querySelector('.post1');
+const post2 = document.querySelector('.post2');
+const post3 = document.querySelector('.post3');
+const posts = [post1, post2, post3];
+
+let clickNum = 1;
+let displayedPost = posts[1];
+displayedPost.style.display = 'block';
+
+
+
+
+leftBtn.addEventListener('click', () => {
+    
+    let postNum = Math.abs((clickNum % 3)) + 1;
+    clickNum++;
+    console.log(postNum);
+
+    displayedPost = posts[postNum];
+
+
+})
