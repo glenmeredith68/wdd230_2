f65595f54c49c2e9e597c9fce271325d929c3c10
Glen Meredith <mer16014@byui.edu>
2022-04-06 13:52:57 -0600

    heart buttons

--- temple-inn/js/temples.js
+++ temple-inn/js/temples.js
@@ -5,7 +5,9 @@ fetch(path)
         const temples = data['temples'];
         temples.forEach(temple => {
             let card = document.createElement('section');
-            let h2 = document.createElement('h2');
+            let title = document.createElement('div');
+            let heart = document.createElement('img');
+            let h3 = document.createElement('h3');
             let img = document.createElement('img');
             let imgDiv = document.createElement('div');
             let address1 = document.createElement('a');
@@ -14,13 +16,68 @@ fetch(path)
             let phone = document.createElement('a');
             let detailsBtn = document.createElement('p');
             let details = document.createElement('div');
+
             let history = document.createElement('div');
             let services = document.createElement('div');
             let ordSched = document.createElement('p');
             let sessSched = document.createElement('p');
             let closures = document.createElement('div');
 
-            h2.textContent = temple.name;
+            h3.textContent = temple.name;
+            title.appendChild(h3);
+
+            // heart button
+            // let unliked = true;
+            // // function like() {
+            // //     if (unliked) {
+            // //         heart.setAttribute('src', '../images/social/heart-full.png')
+            // //         heart.setAttribute('alt', 'You like this temple');
+            // //         localStorage.setItem(`${temple.name} liked src`, '../images/social/heart-full.png');
+            // //         localStorage.setItem(`${temple.name} liked alt`, 'You like this temple');
+            // //     }
+            // //     else {
+            // //         heart.setAttribute('src', '../images/social/heart-empty.png');
+            // //         heart.setAttribute('alt', 'You can like this temple');
+            // //         localStorage.setItem(`${temple.name} liked src`, '../images/social/heart-empty.png');
+            // //         localStorage.setItem(`${temple.name} liked alt`, 'You can like this temple');
+            // //     }
+            // //     unliked = !unliked;
+            // // }
+            // let likedInPast = localStorage.getItem(`${temple.name} liked`);
+            // console.log(likedInPast);
+            // window.addEventListener('load', () => {
+            //     console.log('loaded');
+            //     if (likedInPast != null) {
+            //         heart.setAttribute('src', localStorage.getItem(`${temple.name} liked src`));
+            //         heart.setAttribute('alt', `${temple.name} liked alt`);
+            //     }
+            //     else {
+            //         console.log('it\'s null');
+            //         heart.setAttribute('src', 'images/social/heart-empty.png');
+            //         heart.setAttribute('alt', 'You can like this temple');
+            //     }
+            // })
+            // heart.addEventListener('click', () => {
+            //     if (unliked) {
+            //         heart.setAttribute('src', 'images/social/heart-full.png');
+            //         heart.setAttribute('alt', 'You like this temple');
+            //         localStorage.setItem(`${temple.name} liked`, 'yes');
+            //         localStorage.setItem(`${temple.name} liked src`, 'images/social/heart-full.png');
+            //         localStorage.setItem(`${temple.name} liked alt`, 'You like this temple');
+            //     }
+            //     else {
+            //         heart.setAttribute('src', '../images/social/heart-empty.png');
+            //         heart.setAttribute('alt', 'You can like this temple');
+            //         localStorage.setItem(`${temple.name} liked`, 'no');
+            //         localStorage.setItem(`${temple.name} liked src`, 'images/social/heart-empty.png');
+            //         localStorage.setItem(`${temple.name} liked alt`, 'You can like this temple');
+            //     }
+            //     unliked = !unliked;
+            // })
+            title.appendChild(heart);
+            title.setAttribute('class', 'card-title');
+
+            // end heart button
 
             img.setAttribute('src', temple.img);
             img.setAttribute('alt', temple.name);
@@ -35,9 +92,11 @@ fetch(path)
             address2.setAttribute('href', temple.addressurl);
             addressDiv.appendChild(address1);
             addressDiv.appendChild(address2);
+            addressDiv.setAttribute('class', 'address');
 
             phone.textContent = temple.phone;
             phone.setAttribute('href', `tel:+${temple.phone}`);
+            phone.setAttribute('class', 'phone');
 
             history.textContent = 'History';
             historyArray = temple.history;
@@ -75,14 +134,15 @@ fetch(path)
             details.appendChild(closures);
             details.setAttribute('class', 'details')
 
-            detailsBtn.textContent = 'Details';
+            detailsBtn.innerHTML = 'Details &#8675;';
             detailsBtn.setAttribute('class', 'details-btn');
             detailsBtn.addEventListener('click', () => {
                 details.classList.toggle('show');
+                card.classList.toggle('outline');
+                detailsBtn.classList.toggle('heavy');
             })
 
-
-            card.appendChild(h2);
+            card.appendChild(title);
             card.appendChild(imgDiv);
             card.appendChild(addressDiv);
             card.appendChild(phone)
@@ -90,6 +150,40 @@ fetch(path)
             card.appendChild(details);
 
             document.querySelector('.temples').appendChild(card);
+
+            // heart btn
+            let unliked = true;
+            let likedInPast = localStorage.getItem(`${temple.name} liked`);
+            console.log(likedInPast);
+            window.addEventListener('load', () => {
+                console.log('loaded');
+                if (likedInPast != null) {
+                    heart.setAttribute('src', localStorage.getItem(`${temple.name} liked src`));
+                    heart.setAttribute('alt', `${temple.name} liked alt`);
+                }
+                else {
+                    console.log('it\'s null');
+                    heart.setAttribute('src', 'images/social/heart-empty.png');
+                    heart.setAttribute('alt', 'You can like this temple');
+                }
+            })
+            heart.addEventListener('click', () => {
+                if (unliked) {
+                    heart.setAttribute('src', 'images/social/heart-full.png');
+                    heart.setAttribute('alt', 'You like this temple');
+                    localStorage.setItem(`${temple.name} liked`, 'yes');
+                    localStorage.setItem(`${temple.name} liked src`, 'images/social/heart-full.png');
+                    localStorage.setItem(`${temple.name} liked alt`, 'You like this temple');
+                }
+                else {
+                    heart.setAttribute('src', '../images/social/heart-empty.png');
+                    heart.setAttribute('alt', 'You can like this temple');
+                    localStorage.setItem(`${temple.name} liked`, 'no');
+                    localStorage.setItem(`${temple.name} liked src`, 'images/social/heart-empty.png');
+                    localStorage.setItem(`${temple.name} liked alt`, 'You can like this temple');
+                }
+                unliked = !unliked;
+            })
         });
         
     })
