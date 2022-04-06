fa3865a764b087fd1825024eacebeda1bf1102e4
Glen Meredith <mer16014@byui.edu>
2022-04-06 03:11:02 -0600

    cards work

---
+++ temple-inn/js/temples.js
@@ -0,0 +1,100 @@
+const path = 'js/temples.json';
+fetch(path)
+    .then(response => response.json())
+    .then(data => {
+        const temples = data['temples'];
+        temples.forEach(temple => {
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let img = document.createElement('img');
+            let imgDiv = document.createElement('div');
+            let address1 = document.createElement('a');
+            let address2 = document.createElement('a');
+            let addressDiv = document.createElement('div');
+            let phone = document.createElement('a');
+            let detailsBtn = document.createElement('p');
+            let details = document.createElement('div');
+            let history = document.createElement('div');
+            let services = document.createElement('div');
+            let ordSched = document.createElement('p');
+            let sessSched = document.createElement('p');
+            let closures = document.createElement('div');
+
+            h2.textContent = temple.name;
+
+            img.setAttribute('src', temple.img);
+            img.setAttribute('alt', temple.name);
+            img.setAttribute('loading', 'lazy');
+            img.setAttribute('width', 350),
+                img.setAttribute('height', 233),
+                imgDiv.appendChild(img);
+
+            address1.textContent = temple.address1;
+            address1.setAttribute('href', temple.addressurl);
+            address2.textContent = temple.address2;
+            address2.setAttribute('href', temple.addressurl);
+            addressDiv.appendChild(address1);
+            addressDiv.appendChild(address2);
+
+            phone.textContent = temple.phone;
+            phone.setAttribute('href', `tel:+${temple.phone}`);
+
+            history.textContent = 'History';
+            historyArray = temple.history;
+            historyArray.forEach(element => {
+                let line = document.createElement('p');
+                line.textContent = element;
+                history.appendChild(line);
+            });
+
+            services.textContent = 'Services';
+            servicesArray = ['clothing', 'cafeteria', 'distribution'];
+            servicesObj = temple.services;
+            servicesArray.forEach(service => {
+                let line = document.createElement('p')
+                line.textContent = `Are ${service} services available? ${servicesObj[service]}`
+                services.appendChild(line);
+            });
+
+            ordSched.textContent = `Ordinance Schedule: ${temple.ordinance}`;
+
+            sessSched.textContent = `Session Schedule: ${temple.session}`;
+
+            closures.textContent = 'Closures';
+            closuresArray = temple.closures;
+            closuresArray.forEach(closure => {
+                let line = document.createElement('p');
+                line.textContent = closure;
+                closures.appendChild(line);
+            });
+
+            details.appendChild(history);
+            details.appendChild(services);
+            details.appendChild(ordSched);
+            details.appendChild(sessSched);
+            details.appendChild(closures);
+            details.setAttribute('class', 'details')
+
+            detailsBtn.textContent = 'Details';
+            detailsBtn.setAttribute('class', 'details-btn');
+            detailsBtn.addEventListener('click', () => {
+                details.classList.toggle('show');
+            })
+
+
+            card.appendChild(h2);
+            card.appendChild(imgDiv);
+            card.appendChild(addressDiv);
+            card.appendChild(phone)
+            card.appendChild(detailsBtn);
+            card.appendChild(details);
+
+            document.querySelector('.temples').appendChild(card);
+        });
+        
+    })
+
+// let details = document.querySelector('.details');
+// details.addEventListener('click', () => {
+//     details.children.classList.toggle('show');
+// })
